5bf6b7d06f2f103e40efa59b5c609f371a6eedb7
TatianaVZ <ms.zadvorina@bk.ru>
2022-11-11 20:33:55 +1100

    урок922

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,7 @@ public class Main {
 
         System.out.println("Задание 9.1.3.");
         double[] fruits = new double[]{3.0, 2.5, 5.2, 3.3};
-        for (int i = 0; i < fruits.length; i++)
-            System.out.print(fruits[i] + ", ");
+        for (double fruit : fruits) System.out.print(fruit + ", ");
 
         System.out.println();
 
@@ -46,16 +45,14 @@ public class Main {
         System.out.println("Задание 9.4 ");
 
         int[] numbersPositive = {1, 2, 3, 4, 5};
-        int i = 0;
-        int total = 0;
-        for (i = 0; i < numbersPositive.length; i++) {
-            if (numbersPositive[i] % 2 == 0) {
-                total = numbersPositive[i] + 1;
+        for (int j : numbersPositive) {
+            if (j % 2 == 0) {
+                int total = j + 1;
                 {
                     System.out.print(total + ", ");
                 }
             }
         }
-        System.out.println("");
+        System.out.println();
     }
 }
